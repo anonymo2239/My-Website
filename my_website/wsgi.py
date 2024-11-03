5383ea5ae573994ce80d985f91fe17f55db40afa
anonymo2239 <alperen.arda.adem22@gmail.com>
2024-11-03 18:32:10 +0300

    .

--- my_website/wsgi.py
+++ my_website/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for my_website project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# Django projesinin yolunu ekleyin
+path = '/var/www/html'
+if path not in sys.path:
+    sys.path.insert(0, path)
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_website.settings')
+# Settings modülünü açıkça belirtin
+os.environ['DJANGO_SETTINGS_MODULE'] = 'my_website.settings'
 
 application = get_wsgi_application()
