b697ce1e56e9f3887d0337aa5a4a15860b0e590d
anonymo2239 <alperen.arda.adem22@gmail.com>
2025-01-01 15:53:54 +0300

    Update settings and cache file

--- my_website/settings.py
+++ my_website/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=%8-k4q2#*+mxec8nfhd3wuzz3e5y^0(f-mr$o_0d-fue4n13g'
+from decouple import config
+
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
